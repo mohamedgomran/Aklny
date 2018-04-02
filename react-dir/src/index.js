98bb2a638d0e02e9be84fecd725decc7bea4f5c2
georgesamir21 <georges.samir21@gmail.com>
2018-04-03 01:23:35 +0200

    friends page react

--- react-dir/src/index.js
+++ react-dir/src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
+
+import Friends from './friends.js';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class App extends React.Component{
+    render(){
+        return(
+                <Router>
+                    <div>
+                        <Link to="/friends">Friends</Link>
+                        <Route path="/friends" Component={Friends} />
+                    </div>
+                </Router>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
