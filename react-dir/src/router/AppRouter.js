b4af655426098b2c28f5ebd3636a0a3b291fe59a
Mohamed Omran <mohamedgomran@gmail.com>
2018-04-03 21:30:09 +0200

    header with routing

--- react-dir/src/router/AppRouter.js
+++ react-dir/src/router/AppRouter.js
@@ -4,14 +4,9 @@ import Friends from '../components/Friends';
 import Home from '../components/Home';
 import Header from '../components/Header';
 
-const HeadLinks = ()=>(
-    <NavLink to="/friends" activeClassName="active">Friends</NavLink>
-    
-)
 const AppRouter =()=> (
     <BrowserRouter>
         <div>
-            <HeadLinks/>
             <Header />
             <Switch>
                 <Route path="/" component={Home} exact={true}/>
