52ca6d428be1a992676f328bb28fdaca1e0f0ccc
Ahmed-Hamdy2018 <mardwow2012@gmail.com>
2018-04-04 12:04:52 +0200

    react semantic table Orders

--- react-dir/src/components/Orders.js
+++ react-dir/src/components/Orders.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import { Button, Confirm ,Table} from 'semantic-ui-react'
 
 
 class Orders extends React.Component {
 
   constructor(props){
    super(props);
-   this.state = {size: 3}
+   this.state = {size: 3,
+        open: false,
+        result: 'show the modal to capture a result'
+   }
  }
 
-  render() {
+ show = () => this.setState({open: true })
+ handleConfirm = () => this.setState({ result: 'confirmed', open: false })
+ handleCancel = () => this.setState({ result: 'cancelled', open: false })
 
+  render() {
+    const { open, result } = this.state
     let rows = [];
     for (var i = 0; i < this.state.size; i++){
       let rowID = `row${i}`
@@ -21,44 +29,64 @@ class Orders extends React.Component {
       rows.push(<tr key={i} id={rowID}>{cell}</tr>)
     }
     return(
-      <div className="container" class="ui grid centered">
-      <div className="four column row">
-          <div class="left floated column"><h2>Orders</h2></div>
-          <div class="right floated column">
-              <button class="ui right floated button">Start New Order</button>
-          </div>
-      </div>
-      <div className="row centered">
-        <div class="row">
-        <table class="ui selectable inverted table ten wide column">
-            <thead>
-             <tr>
-               <th>Order</th>
-               <th>Resturnats</th>
-               <th>Invited</th>
-               <th>Joined</th>
-               <th>Status</th>
-               <th class="five wide">Actions</th>
-             </tr>
-           </thead>
-           <tbody>
-             <tr>
-                <td>breakfast</td>
-                <td>mac</td>
-                <td>5</td>
-                <td>10</td>
-                <td>waiting</td>
-                <td>
+      <Table size='small'>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell >Order</Table.HeaderCell>
+        <Table.HeaderCell >Resturnats</Table.HeaderCell>
+        <Table.HeaderCell >Invited</Table.HeaderCell>
+        <Table.HeaderCell >Joined</Table.HeaderCell>
+        <Table.HeaderCell >Status</Table.HeaderCell>
+        <Table.HeaderCell width={5} >Actions</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell>John</Table.Cell>
+        <Table.Cell>mac</Table.Cell>
+        <Table.Cell>5</Table.Cell>
+        <Table.Cell>6</Table.Cell>
+        <Table.Cell>waiting</Table.Cell>
+        <Table.Cell>
+        <button class="mini ui active button">View</button>
+        <button class="mini positive ui button" id="btnsuccessRight">Finish</button>
+        <button class="mini negative ui button" onClick={this.show} id="btnDangerRight">
+        Cancel
+        </button>
+        </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>John</Table.Cell>
+            <Table.Cell>mac</Table.Cell>
+            <Table.Cell>5</Table.Cell>
+            <Table.Cell>6</Table.Cell>
+            <Table.Cell>waiting</Table.Cell>
+            <Table.Cell>
+            <button class="mini ui active button">View</button>
+            <button class="mini positive ui button" id="btnsuccessRight">Finish</button>
+            <button class="mini negative ui button" onClick={this.show} id="btnDangerRight">
+            Cancel
+            </button>
+            </Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>John</Table.Cell>
+                <Table.Cell>mac</Table.Cell>
+                <Table.Cell>5</Table.Cell>
+                <Table.Cell>6</Table.Cell>
+                <Table.Cell>waiting</Table.Cell>
+                <Table.Cell>
                 <button class="mini ui active button">View</button>
-                <button class="mini positive ui button">Finish</button>
-                <button class="mini negative ui button">Cancel</button>
-                </td>
-              </tr>
-           </tbody>
-         </table>
-      </div>
-    </div>
-  </div>
+                <button class="mini positive ui button" id="btnsuccessRight">Finish</button>
+                <button class="mini negative ui button" onClick={this.show} id="btnDangerRight">
+                Cancel
+                </button>
+                </Table.Cell>
+                  </Table.Row>
+        </Table.Body>
+      </Table>
+
     );
   }
 }
