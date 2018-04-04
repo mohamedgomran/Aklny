dadc9fedcc10918aa2c7744d5d1481d010b15df0
HagarAbdelwahab <hagarabdelwahab@outlook.com>
2018-04-04 10:49:21 +0200

    login and register react

---
+++ react-dir/src/components/Register.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import GridColumn, { Button, Form, Message, Icon, Label, Input, Grid, Header, Image, Segment} from 'semantic-ui-react'
+
+
+class Register extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {username: '',password:'',Email:'',confirmpass:'',errmsg:''};
+
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePass = this.handleChangePass.bind(this);
+    this.handleChangeconfirm = this.handleChangeconfirm.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleChangeName(event) {
+    this.setState({username: event.target.value});
+  }
+
+
+  handleChangeEmail(event) {
+    this.setState({Email: event.target.value});
+   
+  }
+
+
+  handleChangePass(event) {
+    this.setState({password: event.target.value});
+  }
+
+
+  handleChangeconfirm(event) {
+    this.setState({confirmpass: event.target.value});
+  }
+
+
+  handleSubmit(event) {
+    console.log('register data: ' + this.state.username+this.state.password+' '+this.state.confirmpass+' '+this.state.Email);
+     if(this.state.password != this.state.confirmpass)
+     {
+         this.setState({errmsg:'invalid Password'});
+     }else{
+       //send data to backend
+     }
+    event.preventDefault();
+  }
+
+
+  render() {
+    return (
+        
+      <div  className='login-form'>
+      <Grid
+      textAlign='center'
+      style={{ height: '100%' }}
+      verticalAlign='middle'
+      >
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as='h2' color='teal' textAlign='center'>
+          {/* <Image src='/logo.png' /> */}
+          {' '}Registeration
+        </Header>
+        <Form size='large' onSubmit={this.handleSubmit}>
+          <Segment stacked>
+          <Form.Input
+              fluid
+              icon='user'
+              iconPosition='left'
+              placeholder='User Name'
+              value={this.state.username} onChange={this.handleChangeName} required 
+            />
+
+            <Form.Input
+              fluid
+              icon='user'
+              iconPosition='left'
+              placeholder='E-mail address'
+              value={this.state.Email} onChange={this.handleChangeEmail} required 
+            />
+
+            <Form.Input
+              fluid
+              icon='lock'
+              iconPosition='left'
+              placeholder='Password'
+              type='password'
+              value={this.state.password} onChange={this.handleChangePass} required
+            />
+
+            <Form.Input
+              fluid
+              icon='lock'
+              iconPosition='left'
+              placeholder='Confirm Password'
+              type='password'
+              value={this.state.confirmpass} onChange={this.handleChangeconfirm} required
+            />
+
+            <Button color='teal' fluid size='large'>Register</Button>
+          </Segment>
+        </Form>
+        <label>
+        {this.state.errmsg}
+        </label>
+      </Grid.Column>
+    </Grid>
+      
+      {/* <form success onSubmit={this.handleSubmit}>
+      
+           <Input label='Name'  type='text' placeholder='User Name' value={this.state.username} onChange={this.handleChangeName} required />
+            <br/>
+            <br/>
+           <Input label='Email'  type='email' placeholder='joe@schmoe.com' value={this.state.Email} onChange={this.handleChangeEmail} required />
+            <br/>
+            <br/>
+            <Input label='Password ' type="password" value={this.state.password} onChange={this.handleChangePass} required/>
+            <br/>
+            <br/>
+            <Input label='Confirm Password ' type="password" value={this.state.confirmpass} onChange={this.handleChangeconfirm} required/>
+            <br/>
+            <br/>
+            <Button color='blue'>Register</Button>
+            <br/>
+            <br/>
+            <label basic color='red' pointing>{this.state.errmsg}</label>
+      </form>
+       */}
+      
+
+    </div>
+  
+    );
+  }
+}
+
+export default Register;
--- react-dir/src/router/AppRouter.js
+++ react-dir/src/router/AppRouter.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Route, Link, NavLink, Switch } from "react-router-dom";
 import Friends from '../components/friends';
 import Orders from '../components/Orders';
 import Home from '../components/Home';
-
+import Login from '../components/Login';
+import Register from '../components/Register';
 const HeadLinks = ()=>(
     <NavLink to="/friends" activeClassName="active">Friends</NavLink>
 
@@ -13,9 +14,11 @@ const AppRouter =()=> (
         <div>
             <HeadLinks/>
             <Switch>
-                <Route path="/" component={Home} exact={true}/>
-                <Route path="/friends" component={Friends}/>
+                  <Route path="/" component={Home} exact={true}/>
+                  <Route path="/friends" component={Friends}/>
                   <Route path="/Orders" component={Orders}/>
+                  <Route path="/Login" component={Login}/>
+                  <Route path="/Register" component={Register}/>
             </Switch>
         </div>
     </BrowserRouter>
