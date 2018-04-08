b9d732b4dffbf9d98c920c607001601470b5b47c
HagarAbdelwahab <hagarabdelwahab@outlook.com>
2018-04-09 00:29:39 +0200

    logout

--- react-dir/src/components/Register.js
+++ react-dir/src/components/Register.js
@@ -83,6 +83,7 @@ export default class Register extends Component {
                    
                 }else
                 {
+                  
                   <Redirect to="/login"/>
                 }
                
