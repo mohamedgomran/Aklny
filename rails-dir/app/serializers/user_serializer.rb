1fd4cb35828d915388551a02d1b6e6fab85ef8b4
HagarAbdelwahab <hagarabdelwahab@outlook.com>
2018-04-08 19:26:50 +0200

    auth_token

--- rails-dir/app/serializers/user_serializer.rb
+++ rails-dir/app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   # attributes :id
-  attributes :id, :email, :username, :role, :created_at, :updated_at, :last_login
+  attributes :id, :email, :name, :created_at, :updated_at, :last_login
 end
