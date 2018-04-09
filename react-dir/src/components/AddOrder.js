f3c98d09022c6a36106ad9ab2c42b5110447d07f
Mohamed Omran <mohamedgomran@gmail.com>
2018-04-09 22:01:10 +0200

    add order

--- react-dir/src/components/AddOrder.js
+++ react-dir/src/components/AddOrder.js
@@ -4,69 +4,62 @@ import axios from 'axios';
 import GroupsAPI from '../API/groups-api';
 import GroupMember  from './GroupMembers';
 import FileBase64 from 'react-file-base64';
+var uuid = require('uuid-v4');
 
 let headers = {
-	'Content-Type': 'application/json',
-	 'Authorization': 'Bearer ' + localStorage.getItem('token')
-	}
-var uuid = require('uuid-v4');
+    'Content-Type': 'application/json',
+     'Authorization': 'Bearer ' + localStorage.getItem('token')
+    }
 
 
 export default class AddOrder extends React.Component{
 
   constructor(props) {
     super(props);
-
-
 		this.handleChangeRes = this.handleChangeRes.bind(this);
 		this.handleChangeType = this.handleChangeType.bind(this);
 		this.handleChangeName = this.handleChangeName.bind(this);
-
-  }
+    }
 
 	state = {
 			activeItem: '',
 			status: '',
 			res_name:'',
-			order_for:'',
+			order_for:'Breakfast',
 			invitedFriends: [],
 			invited:[],
 			friends : [] ,
 			groups :[],
 			members :[],
+            publish:true
 
 	}
 
 	componentDidMount(){
-
 		this.getallFriends()
 		GroupsAPI.getAllGroups((res)=>{
-				this.setState({groups:res})
-				console.log(this.state.groups);
+			this.setState({groups:res})
+			console.log(this.state.groups);
 		})
-
 	}
 
 	handleItemClick = (e, { key }) => {
-			console.log("My Active Item is ",key)
-			this.state.activeItem = key
-
-
-			GroupsAPI.listMembers(key, (res)=>{
-					if (res.success) {
-							console.log(res.message)
-							this.setState(prevState=>{
-									return {groupMembers:res.message}
-							})
-					}
-			})
+		console.log("My Active Item is ",key)
+		this.state.activeItem = key
+		GroupsAPI.listMembers(key, (res)=>{
+			if (res.success) {
+				console.log(res.message)
+				this.setState(prevState=>{
+						return {groupMembers:res.message}
+				})
+			}
+        })
 
 	}
 
     submit = (e)=>{
         e.preventDefault();
         console.log(e.target.elements);
-
     }
 
     handleChangeType(event) {
@@ -76,7 +69,6 @@ export default class AddOrder extends React.Component{
 
     handleChangeRes(event) {
       this.setState({'res_name': event.target.value});
-
     }
 
 
@@ -88,8 +80,6 @@ export default class AddOrder extends React.Component{
       console.log('image',event.target.files[0]);
     }
 
-
-
     getallFriends = ()=>{
          axios.get(`http://localhost:3000/users/friends`, {
              headers: headers
@@ -99,55 +89,47 @@ export default class AddOrder extends React.Component{
 						 console.log(this.state.groups);
          }).catch((error)=>{
              console.log("error", error);
-
         })
-
   }
 
-
-
-  addOrder = ()=>{
-    this.setState( ()=>{
+    addOrder = ()=>{
+        this.setState( ()=>{
         axios.post(`http://localhost:3000/orders`,{
-						order_for:this.state.order_for,
+    		order_for:this.state.order_for,
             res_name: this.state.res_name,
-						invited : this.state.invited,
-                        menu: this.state.menu
+    		invited : this.state.invited,
+            menu: this.state.menu
         },
         {headers: headers }).then((response)=>{
             console.log("response",response);
 
         }).catch((error)=>{
             console.log("error", error);
-						console.log(this.user);
+    		console.log(this.user);
         })
-
     })
-			console.log(this.state.invited);
-  }
+    console.log(this.state.invited);
+    }
 
 
     inviteFriend = (e)=>{
         console.log("befor",this.state.invitedFriends)
-            let invitedFriendsArr = this.state.invitedFriends.slice();
-		            let invite = this.state.invited.slice();
-            this.state.friends.forEach(friend=>{
-                if(e.target.value == friend.id){
-										console.log(e.target.value);
-									if(invitedFriendsArr.indexOf(e.target.value) == -1){
-
-										let invitedFriend = friend;
-										invitedFriendsArr.push(invitedFriend);
-										invite.push(invitedFriend.id);
-										console.log(invitedFriend.id);
-
-									}
-
-                }
-            })
-            this.setState({invitedFriends:invitedFriendsArr})
-					  this.setState({invited:invite})
-						console.log(this.state.invited);
+        let invitedFriendsArr = this.state.invitedFriends.slice();
+        let invite = this.state.invited.slice();
+        this.state.friends.forEach(friend=>{
+            if(e.target.value == friend.id){
+			console.log(e.target.value);
+				if(invitedFriendsArr.indexOf(e.target.value) == -1){
+					let invitedFriend = friend;
+					invitedFriendsArr.push(invitedFriend);
+					invite.push(invitedFriend.id);
+					console.log(invitedFriend.id);
+				}
+            }
+        })
+        this.setState({invitedFriends:invitedFriendsArr})
+	    this.setState({invited:invite})
+		console.log(this.state.invited);
     }
 
 
@@ -161,14 +143,12 @@ export default class AddOrder extends React.Component{
         this.setState({invitedFriends:newInvitedArr}, ()=>{
             console.log(this.state.invitedFriends);
         })
-
-
     }
 
-  getFiles = (file)=>{
-    console.log(file.base64)
-    this.setState({ menu: file.base64 })
-  }
+    getFiles = (file)=>{
+        console.log(file.base64)
+        this.setState({ menu: file.base64 })
+    }
 
     render = ()=>{
         return (
@@ -180,13 +160,13 @@ export default class AddOrder extends React.Component{
 
                     <Form.Field onChange={this.handleChangeType} value={this.state.order_for} id ='add_order'>
                         <select name="meal" className="ui search dropdown" placeholder="Select Meal">
-                            <option value="Lunch" >Lunch</option>
                             <option value="Breakfast" >Breakfast</option>
+                            <option value="Lunch" >Lunch</option>
                         </select>
                     </Form.Field>
 
                      <Form.Field>
-                        <input type="text" name="restaurant" placeholder="Restaurant Name" onChange={this.handleChangeRes} value={this.state.res_name} />
+                        <input required type="text" name="restaurant" placeholder="Restaurant Name" onChange={this.handleChangeRes} value={this.state.res_name} />
                      </Form.Field>
 
                      <Form.Field>
@@ -199,23 +179,21 @@ export default class AddOrder extends React.Component{
                                 )
                             })
                         }
-												{
-														this.state.groups.map(group =>{
-																return(
-																		<option key={group.id} value={group.id} onClick={this.handleItemClick} >{group.name}</option>
-																)
-														})
-												}
-										</select>
+						{
+							this.state.groups.map(group =>{
+								return(
+									<option key={group.id} value={group.id} onClick={this.handleItemClick} >{group.name}</option>
+								)
+							})
+						}
+						</select>
                     </Form.Field>
                     <Form.Field>
                         <FileBase64 multiple={ false } onDone={ this.getFiles }/>
                     </Form.Field>
-                    <Button basic color="green" size="large" floated="right">Puplish</Button>
+                    <Button basic color="green" size="large" floated="right">Publish</Button>
                 </Form>
-
-                    </Grid.Column>
-
+                </Grid.Column>
 
                    <Grid.Column>
                     <h4>Invited Friends</h4>
@@ -235,7 +213,7 @@ export default class AddOrder extends React.Component{
                                 )
                             })
                         }
-												{this.props.match.params.key && <Segment>
+						{this.props.match.params.key && <Segment>
                             <GroupMember groupId={this.state.activeItem} />
                             </Segment>
                         }
